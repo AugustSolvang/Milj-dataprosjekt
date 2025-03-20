a7de697c8d71b6cb5a310ebc1e7caa2d42501eba
AugustSolvang <august.sol2005@gmail.com>
2025-03-20 14:17:13 +0100

    Ferdig Weather_Data_Met.py

--- data/Weather_Data_MET.py
+++ data/Weather_Data_MET.py
@@ -1,14 +1,46 @@
-import requests
-import pandas as pd
 from dotenv import load_dotenv
 import os
+import requests
+import pandas as pd
+from datetime import date
+
 
 load_dotenv()
 
-api_key = os.getenv("API_Key_MET")
-url = os.getenv("MET_URL")
+def fetch_data(Api_Key, Url, Sources, Elements):
+    Start_Year = 1900
+    End_Year = date.today().year
+    data_list = []
+    Added_Dates = set()
 
+    for year in range(Start_Year, End_Year + 1):
+        Start_Date = f"{year}-01-01"
+        End_Date = f"{year}-12-31"
+        Reference_Time = f"{Start_Date}/{End_Date}"
+        Endpoint = f"{Url}?sources={Sources}&elements={Elements}&referencetime={Reference_Time}"
+        res = requests.get(Endpoint, auth = (Api_Key, ""))
+        
+        if res.status_code == 200:
+            data = res.json()
+            for observation in data.get("data", []):
+                Date = observation["referenceTime"][:10]
+                if Date not in Added_Dates:
+                    Value = observation["observations"][0]["value"]
+                    data_list.append([Date, Value])
+                    Added_Dates.add(Date)
+        else:
+            print("Error. Statuscode er ikke lik 200")
+            print(res.status_code)
+        
+        df = pd.DataFrame(data_list, columns = ["Date", "Temp[°C]"])
+        print(df)
 
-print(api_key)
-print(url)
+Api_Key = os.getenv("API_Key_MET")
+Url = os.getenv("Base_MET_URL")
+Sources = "SN18700"
+Elements = "air_temperature"
+
+if __name__ == "__main__":
+    df = fetch_data(Api_Key, Url, Sources, Elements)
+    print(df)
 
