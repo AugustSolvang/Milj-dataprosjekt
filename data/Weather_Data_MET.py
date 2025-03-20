662528356fe8ee7968f8ae038ec30c11c722e3ed
AugustSolvang <august.sol2005@gmail.com>
2025-03-20 13:05:44 +0100

    oppdatert .env

--- data/Weather_Data_MET.py
+++ data/Weather_Data_MET.py
@@ -1,47 +1,14 @@
 import requests
 import pandas as pd
+from dotenv import load_dotenv
+import os
 
-api_key = "f6ece757-0cb9-40c7-87cf-7817a5e6bb70"
-url = "https://frost.met.no/observations/v0.jsonld"
+load_dotenv()
 
-headers = {
-    "Authorization": f"Bearer {api_key}"
-}
-
-params = {
-    "sources": "SN18700",
-    "referencetime": "2000-01-01/2017-02-01",
-    "elements": "air_temperature"
-}
-
-response = requests.get(url, headers=headers, params=params, auth=(api_key, ""))
-
-if response.status_code == 200:
-    data = response.json()
-
-    observations = []
-    observations = []
-    for entry in data['data']:
-        for obs in entry['observations']:
-            observations.append({
-                'Dato': entry['referenceTime'],
-                'Temperatur (°C)': obs['value']
-            })
-
-    # Konvertere til Pandas DataFrame
-    df = pd.DataFrame(observations)
-
-    # Konvertere dato til riktig format
-    df['Dato'] = pd.to_datetime(df['Dato']).dt.date  # Kun dato, ikke klokkeslett
-
-    # Beregne daglig gjennomsnittstemperatur
-    df_daily = df.groupby('Dato', as_index=False)['Temperatur (°C)'].mean()
-
-    # Vise resultatet
-    print(df_daily)
+api_key = os.getenv("API_Key_MET")
+url = os.getenv("MET_URL")
 
 
-else:
-    print(response.status_code)
-    print(response.url)
+print(api_key)
+print(url)
 
