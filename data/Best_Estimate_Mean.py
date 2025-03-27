f0d86f2cb40ff88ddee3722db13e7d0655412f44
AugustSolvang <august.sol2005@gmail.com>
2025-03-27 14:23:00 +0100

    Laget kode som oppretter json med temp avvik fra normen 1961-1990

--- data/Best_Estimate_Mean.py
+++ data/Best_Estimate_Mean.py
@@ -19,16 +19,11 @@ def fetch_data(Filename, Api_Key, Url, Sources, Elements, Reference_Time):
 
 load_dotenv()
 
-Filename = "Mean_Air_Temp_MET_1960-1990.json"
+Filename = "Air_Temp_Anomaly_1960-1990.json"
 Api_Key = os.getenv("API_Key_MET")
 Url = os.getenv("Base_MET_URL")
 Sources = "SN18700"
 Elements = "best_estimate_mean(air_temperature_anomaly P1M 1961_1990)"
-Reference_Time = "1960-01-01/1991-01-01"
-
-fetch_data(Filename, Api_Key, Url, Sources, Elements, Reference_Time)
-
-Filename = "Mean_Air_Temp_MET_1991-2020.json"
-Reference_Time = "1991-01-01/2021-01-01"
+Reference_Time = "1960-01-01/2025-01-01"
 
 fetch_data(Filename, Api_Key, Url, Sources, Elements, Reference_Time)
