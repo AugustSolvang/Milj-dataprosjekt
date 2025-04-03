63594e1bb1f240bbe1dacbe1fcd646ae7f5c0871
AugustSolvang <august.sol2005@gmail.com>
2025-04-03 14:53:27 +0200

    Lagt til data for nedbør

--- data/Json_Dump_MET.py
+++ data/Json_Dump_MET.py
@@ -19,11 +19,11 @@ def fetch_data(Filename, Api_Key, Url, Sources, Elements, Reference_Time):
 
 load_dotenv()
 
-Filename = "Air_Temp_Anomaly_1960-1990.json"
+Filename = "Precipitation_Sum_Anomaly_1961-1990.json"
 Api_Key = os.getenv("API_Key_MET")
 Url = os.getenv("Base_MET_URL")
 Sources = "SN18700"
-Elements = "best_estimate_mean(air_temperature_anomaly P1M 1961_1990)"
-Reference_Time = "1960-01-01/2025-01-01"
+Elements = "best_estimate_sum(precipitation_amount_anomaly P1M 1961_1990)"
+Reference_Time = "1940-01-01/2025-01-01"
 
 fetch_data(Filename, Api_Key, Url, Sources, Elements, Reference_Time)
